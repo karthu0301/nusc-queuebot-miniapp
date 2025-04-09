c5454cc4fc8f17cb99a6b39ef6719aec58d43257
josh1248 <joshthoo@gmail.com>
2025-04-09 17:50:48 +0800

    handling another config issue for cloud

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,12 @@ export default defineConfig(({ mode }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return {
     server: {
-      host: process.env.VITE_HOST,
+      host: process.env.VITE_HOST ?? '0.0.0.0',
       port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     },
     preview: {
-      allowedHosts: [
-        process.env.VITE_CLOUD_HOST ?? '',
-      ],
+      allowedHosts: process.env.VITE_CLOUD_HOST ? [process.env.VITE_CLOUD_HOST] : [],
+      host: '0.0.0.0',
     },
   };
 });
